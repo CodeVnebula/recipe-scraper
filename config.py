c286c35da0f84fbdb522f192852379fdbaba2485
CodeVnebula <chkhikvadzem471@gmail.com>
2024-09-26 01:23:33 +0400

    Highlight potential issues

--- config.py
+++ config.py
@@ -7,4 +7,8 @@ MONGO_URI = os.getenv('MONGO_URI', 'mongodb://localhost:27017')
 DATABASE_NAME = "recipe_db"
 
 KULINARIA_URL = "https://www.kulinaria.ge"
+
+# KULINARIA_RECIPE_URL = KULINARIA_URL / "receptebi/"
+# '/' Throws an error because it's a string
+# KULINARIA_RECIPE_URL = KULINARIA_URL + "/receptebi/..." 
 KULINARIA_RECIPE_CATEGORY_URL = KULINARIA_URL / "receptebi/cat/comeuli/"
