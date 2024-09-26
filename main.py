fc7d3dad7ad07178dc52e09d9be33ff22666ee1e
Luka Goguadze <lukagoguadze2@gmail.com>
2024-09-26 18:41:04 +0400

    The scraper now skips data that already exists in the database.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import time
 import asyncio
 
 from models.recipe import ConcreteRecipe
-from database.mongo import MongoDB
+from database import MongoDB
 from config import MONGO_URI, DATABASE_NAME
 from scraper import RecipeScraper
 
@@ -13,7 +13,7 @@ from config import COMEULI_CATEGORY_URL
 async def main():
     db = MongoDB(uri=MONGO_URI, db_name=DATABASE_NAME)
     
-    await db.drop_collection()
+    # await db.drop_collection()
 
     _scraper = RecipeScraper(
         category_url=COMEULI_CATEGORY_URL,
@@ -22,9 +22,7 @@ async def main():
 
     start = time.perf_counter()
     recipes_data = await _scraper.scrape_recipes()
-    
-    print(len(result), "recipes scraped")
-    print(f'Scraping took {time.perf_counter() - start} seconds')
+    end_time = time.perf_counter() - start
    
     recipes = []
     for recipe_data in recipes_data:
@@ -34,6 +32,10 @@ async def main():
         print(f"Recipe saved id: {recipe.id}")
         recipes.append(recipe)
 
+    print()
+    print(len(recipes_data), "recipes scraped")
+    print(f"Found {_scraper.exists_in_db} existing recipes in database")
+    print(f'Scraping took {end_time:.2f} seconds')
     print("Success")
     
 if __name__ == '__main__':
--- scraper/scraper.py
+++ scraper/scraper.py
@@ -3,16 +3,22 @@ import asyncio
 
 from bs4 import Tag
 from scraper.parser import MainPageParser, RecipeParser
+from config import DATABASE_NAME, MONGO_URI
+from database import MongoDB
+from utils.helper import get_id_from_url
 
 
 class RecipeScraper:
     def __init__(self, category_url: str, page_limit: int | None = None):
         self.page_limit = page_limit
-        self.category = category_url
+        self.category_url = category_url
         self._page_number = 1
+        self.exists_in_db = 0
         self._all_recipes = []
         self._tasks = []
         self.__lock = asyncio.Lock()
+
+        self.__db = MongoDB(MONGO_URI, DATABASE_NAME)
 
     async def print(self, *args, **kwargs) -> None:
         async with self.__lock:
@@ -29,14 +35,24 @@ class RecipeScraper:
         post_data = MainPageParser.scrape_post(post)
 
         if post_data.get('recipe_url'):
-            await self.print('Scraping post', post_data.get('recipe_url'))
-            detailed_data = await self._fetch_recipe_details(post_data.get('recipe_url'), return_all_data=False)
+            recipe_url = post_data.get('recipe_url')
+
+            if await self.__db.recipe_exists(
+                    get_id_from_url(
+                        recipe_url
+                    )
+            ):
+                self.exists_in_db += 1
+                return None
+
+            detailed_data = await self._fetch_recipe_details(recipe_url, return_all_data=False)
             post_data.update(detailed_data)
 
         return post_data
 
     async def _fetch_recipe_details(self, recipe_url: str | bytes, return_all_data: bool = True) -> dict:
         """Fetch recipe details from the recipe URL."""
+        await self.print('Scraping post', recipe_url)
         try:
             html = await self._fetch_html(recipe_url)
         except aiohttp.client_exceptions:
@@ -55,7 +71,7 @@ class RecipeScraper:
             if soup and soup.next_url:
                 current_page_url = soup.next_url
             else:
-                current_page_url = f"{self.category}/?page={self._page_number}"
+                current_page_url = f"{self.category_url}?page={self._page_number}"
 
             await self.print('Scraping page', current_page_url)
 
@@ -85,6 +101,7 @@ class RecipeScraper:
 
         # Wait for all tasks to complete and gather results
         recipes_data = await asyncio.gather(*self._tasks)
-        self._all_recipes.extend(recipes_data)
+
+        self._all_recipes = [data for data in recipes_data if data]
 
         return self._all_recipes
