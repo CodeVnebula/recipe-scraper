ab80c1651275ebb431ea5e5e2e88e47158c7416e
CodeVnebula <chkhikvadzem471@gmail.com>
2024-10-05 00:43:55 +0400

    Converted categories list into dict

--- config.py
+++ config.py
@@ -9,22 +9,22 @@ DATABASE_NAME = "recipe_db"
 KULINARIA_URL = "https://www.kulinaria.ge"
 COMEULI_CATEGORY_URL = KULINARIA_URL + "/receptebi/cat/comeuli/"
 
-CATEGORIES = [
-    'სალათები',
-    'ცომეული',
-    'ნამცხვრები და ტორტები',
-    'ხორცეული',
-    'წვნიანები',
-    'ქართული სამზარეულო',
-    'თევზი და ზღვის პროდუქტები',
-    'დესერტები და ტკბილეულობა',
-    'სამარხვო/ვეგეტარიანული',
-    'მსოფლიო სამზარეულო',
-    'სადღესასწაულო',
-    'პასტა და ბურღულეული',
-    'სასმელები',
-    'კონსერვი და სოუსები',
-    'აპეტაიზერები და გარნირები',
-    'ბავშვებისთვის',
-    'სხვა',
-]
+CATEGORIES = {
+    'სალათები': '/receptebi/cat/salaTebi/',
+    'ცომეული': '/receptebi/cat/comeuli/',
+    'ნამცხვრები და ტორტები': '/receptebi/cat/namcxvrebi-da-tortebi/',
+    'ხორცეული': '/receptebi/cat/xorceuli/',
+    'წვნიანები': '/receptebi/cat/wvnianebi/',
+    'ქართული სამზარეულო': '/receptebi/cat/karTuli-samzareulo/',
+    'თევზი და ზღვის პროდუქტები': '/receptebi/cat/Tevzi-da-zRvis-produktebi/',
+    'დესერტები და ტკბილეულობა': '/receptebi/cat/desertebi-da-tkbileuloba/',
+    'სამარხვო/ვეგეტარიანული': '/receptebi/cat/samarxvovegetarianuli/',
+    'მსოფლიო სამზარეულო': '/receptebi/cat/msoplio-samzareulo/',
+    'სადღესასწაულო': '/receptebi/cat/sadResaswaulo/',
+    'პასტა და ბურღულეული': '/receptebi/cat/pasta-da-burRuleuli/',
+    'სასმელები': '/receptebi/cat/sasmelebi/',
+    'კონსერვი და სოუსები': '/receptebi/cat/konservi-da-sousebi/',
+    'აპეტაიზერები და გარნირები': '/receptebi/cat/apetaizerebi-da-garnirebi/',
+    'ბავშვებისთვის': '/receptebi/cat/bavSvebisTvis/',
+    'სხვა': '/receptebi/cat/sxva/',
+}
